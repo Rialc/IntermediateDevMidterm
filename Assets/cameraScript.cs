30f51b0e1464f957a8af00a162594f98e2fb40f7
Rialc <clair.m.hayden@gmail.com>
2017-10-03 13:12:36 -0400

    InitCommit

---
+++ Assets/cameraScript.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class cameraScript : MonoBehaviour
+{
+    int rot = 0;
+    public LineRenderer LoS;
+    public bool reverse = false;
+    RaycastHit hit;
+    public Text text;
+    int count = 0;
+    // Use this for initialization
+    void Start()
+    {
+        LoS = this.GetComponent<LineRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+        if (!reverse)
+        {
+            rot--;
+            transform.rotation = Quaternion.Euler(31.5f, rot, 0);
+        }
+        else
+        {
+            rot++;
+            transform.rotation = Quaternion.Euler(31.5f, rot, 0);
+        }
+        if (rot == -90)
+        {
+            reverse = true;
+        }
+        else if (rot == 0)
+        {
+            reverse = false;
+        }
+        // Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        Ray ray = new Ray(transform.position, transform.forward);
+        if (Physics.Raycast(ray, out hit))
+        {
+            //Debug.DrawLine(ray.origin, hit.point, Color.red);
+            //Debug.DrawLine(Vector3.zero, new Vector3(1, 0, 0), Color.red);
+            //Debug.Log(hit.point);
+            LoS.SetPosition(0, ray.origin);
+            LoS.SetPosition(1, hit.point);
+            Debug.Log("Camera sees:" + hit.transform.gameObject.name);
+            if (hit.transform.gameObject.name == "Player")
+            {
+                count++;
+                text.text = ("Frames the noob has been hit:" + count);
+            }
+            //target = hit.point;
+        }
+    }
+}
---
+++ Assets/moveToClick.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class moveToClick : MonoBehaviour {
+    RaycastHit hit;
+    public Vector3 target;
+	// Use this for initialization
+	void Start () {
+        target = new Vector3(transform.position.x, 0, transform.position.z);
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.rotation = Quaternion.identity;
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(ray, out hit))
+            {
+                Debug.DrawLine(ray.origin, hit.point);
+                Debug.Log(hit.point + " " + hit.transform.gameObject.name);
+                if (hit.transform.gameObject.tag.Equals("floor"))
+                {
+                    target = hit.point;
+                }
+            }
+        }
+
+        if (target != null && !Input.GetKey(KeyCode.Space))
+        {
+            this.GetComponent<Rigidbody>().MovePosition(Vector3.MoveTowards(transform.position, target + new Vector3(0, 0.75f, 0), .1f));
+        }
+        // transform.position = new Vector3(transform.position.x, 0.75f, transform.position.z);
+    }
+    private void OnCollisionExit(Collision collision)
+    {
+        this.GetComponent<Rigidbody>().velocity = new Vector3(0,0,0);
+    }
+}
